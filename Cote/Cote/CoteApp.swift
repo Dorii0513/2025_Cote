e530cb8a4fd85305ea946beb8f64bddd40614210
Dorii0513 <bobodorii0513@gmail.com>
2025-06-17 02:25:47 +0900

    프로젝트 초기 세팅

---
+++ Cote/Cote/CoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoteApp.swift
+//  Cote
+//
+//  Created by 김예림 on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
