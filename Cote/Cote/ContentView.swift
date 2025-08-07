1a757f4def24c67d68b23a3f7ae6886920d694ef
Dorii0513 <bobodorii0513@gmail.com>
2025-08-07 19:06:31 +0900

    [Design] 블러 효과 수정 및 toolbar 타이틀 생성

--- Cote/Cote/ContentView.swift
+++ Cote/Cote/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("zz")
+        ZStack {
+            Color.bgInputDefault
+            
+            VStack {
+                Text("Untitled")
+                
+                Button {
+                    
+                } label: {
+                    Text("Add tags")
+                }
+            }
         }
     }
 }
