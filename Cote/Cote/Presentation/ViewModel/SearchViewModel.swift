76c06cfd63bd5aba22b002daa39712c63f91a92c
Dorii0513 <bobodorii0513@gmail.com>
2025-11-01 23:26:27 +0900

    [Feature] SearchViewModel 구현

---
+++ Cote/Cote/Presentation/ViewModel/SearchViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  SearchViewModel.swift
+//  Cote
+//
+//  Created by 김예림 on 11/1/25.
+//
+
+import SwiftUI
+import Combine
+
+@MainActor
+final class SearchViewModel: ObservableObject {
+    private let useCase: SearchUseCase
+    
+    @Published var query: String = ""
+    @Published private(set) var results: [SearchResult] = []
+
+    
+    private var cancellables = Set<AnyCancellable>()
+
+    init(usecase: SearchUseCase){
+        self.useCase = usecase
+        observeQuery()
+    }
+    
+    convenience init() {
+        self.init(usecase: DefaultSearchUseCase())
+    }
+
+    // 검색어 실시간 감시
+    private func observeQuery() {
+        $query
+            .debounce(for: .milliseconds(300), scheduler: RunLoop.main) // 과도한 호출 방지
+            .removeDuplicates()
+            .sink { [weak self] newValue in
+                guard let self else { return }
+                Task { await self.search(for: newValue) }
+            }
+            .store(in: &cancellables)
+    }
+
+    func search(for text: String) async {
+        let trimmed = text.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !trimmed.isEmpty else {
+            results = []
+            return
+        }
+
+        do {
+            let res = try await useCase.execute(query: trimmed, topK: 200)
+            results = res
+        } catch {
+            print("❌ 검색 오류: \(error)")
+            results = []
+        }
+    }
+}
