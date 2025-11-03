97368c0594417358968f064db161bbf35777e14a
Dorii0513 <bobodorii0513@gmail.com>
2025-11-03 15:16:57 +0900

    [Test] apple foundation 테스트

---
+++ Cote/Cote/Domain/Usecase/CodeSummarizer.swift
@@ -0,0 +1,8 @@
+//
+//  CodeSummarizer.swift
+//  Cote
+//
+//  Created by 김예림 on 11/3/25.
+//
+
+import Foundation
---
+++ Cote/CoteTests/SummarizerTest.swift
@@ -0,0 +1,8 @@
+//
+//  SummarizerTest.swift
+//  Cote
+//
+//  Created by 김예림 on 11/3/25.
+//
+
+import Foundation
