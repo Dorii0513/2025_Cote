3c55fb82d216537c27b322a0fbfbeaa54a1e1597
Dorii0513 <bobodorii0513@gmail.com>
2025-11-25 23:34:25 +0900

    [Refactor] folderCell 분리

--- Cote/Cote/Presentation/Shared/Cell/Note+FolderCell.swift
+++ Cote/Cote/Presentation/Shared/Cell/Note+FolderCell.swift
@@ -166,29 +166,6 @@ struct ListCell: View {
     }
 }
 
-struct FolderCell: View {
-    @EnvironmentObject private var viewModel: SideBarViewModel
-    
-    let isExpanded: Bool
-    let folder: Folder
-    @Binding var isHover: Bool
-    
-    var body: some View {
-        HStack(spacing: 0) {
-            if isExpanded {
-                Image("arrow_down")
-                    .foregroundStyle(.iconSecondary)
-            } else {
-                Image("arrow_right")
-                    .foregroundStyle(.iconSecondary)
-            }
-            Text(folder.name)
-                .coteFont(.text2,
-                          color: isHover ? .textStrong : .textDefault)
-        }
-    }
-}
-
 extension Notification.Name {
     static let moveNoteRequest = Notification.Name("MoveNoteRequest")
 }
